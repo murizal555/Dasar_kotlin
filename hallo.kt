a9e6ef52ac38eb2d3d4d5cffeedbf9f94678ab88
murizal555 <murizal444.com>
2023-12-05 15:29:36 +0800

    kotlin_fundamental

--- hallo.kt
+++ hallo.kt
@@ -1,3 +1,3 @@
 fun main(){
-    print("pagi")
+    print("pagiiiii")
 }
---
+++ var.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val name="pertama "
+print("nama saya ")
+print(name)
+print(if(true)"selalu benar" else "selalu salah")
+}
