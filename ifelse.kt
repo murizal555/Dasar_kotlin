83c2abc3b259912b64eb0ce7a901f204073fc422
murizal555 <murizal444.com>
2023-12-08 21:56:45 +0800

    if tf

---
+++ ifelse.kt
@@ -0,0 +1,11 @@
+fun main(args:Array<String>){
+val buka=3
+val now=20
+val office:String
+if (now>buka){
+    office="buka"
+}else{
+    office="tutup"
+}
+print(office)
+}
---
+++ ifexp_tf.kt
@@ -0,0 +1,6 @@
+fun main(args:Array<String>){
+  val pertama=7
+  val akhir=20
+  if(pertama > akhir)
+  println("satu")
+}
